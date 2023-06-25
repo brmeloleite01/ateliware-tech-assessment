35c32b615b1d052865250fef4ed105e902193f86
brmeloleite01 <brmelo.leite@gmail.com>
2023-06-25 18:45:07 -0300

    integration with possible pathes api

---
+++ backend/routeOptimization/src/service/PossiblePathesAPI.ts
@@ -0,0 +1,20 @@
+import https from "https";
+import {PossiblePathes} from "../types/PossiblePathes";
+
+const endpoint = "https://mocki.io/v1/10404696-fd43-4481-a7ed-f9369073252f";
+
+export class PossiblePathesAPI {
+  static async fetchAll() : Promise<PossiblePathes> {
+    return new Promise((resolve, reject) => {
+      https.get(endpoint, (res) => {
+        res.on("data", (data) => resolve(data as PossiblePathes));
+
+        res.on("error", (error) => {
+          console.log("Error", error);
+          reject(error);
+        });
+      });
+    });
+  }
+}
+
