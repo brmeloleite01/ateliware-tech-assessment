e8421f1bf5994ba56a948d15770ed2a27fa3116d
brmeloleite01 <brmelo.leite@gmail.com>
2023-06-26 01:37:47 -0300

    RouteOptimization call on index

--- backend/routeOptimization/src/index.ts
+++ backend/routeOptimization/src/index.ts
@@ -1,18 +1,29 @@
 import * as functions from "firebase-functions";
+import express, {Request, Response} from "express";
+import {RouteOptimization} from "./service/RouteOptimization";
+import {ObjectsPosition} from "./types/ObjectsPosition";
+import {PossiblePathesAPI} from "./service/PossiblePathesAPI";
+import {FastestStrategy} from "./service/strategy/FastestStrategy";
+import {Route} from "./types/Route";
 
-import express, { Request, Response} from 'express'
+const service = express();
+service.use(express.json());
 
-const service = express()
+service.post("/fastest", async (request: Request, response: Response ) => {
+  const {drone, start, end} = request.body as ObjectsPosition;
+  const possiblePathes = await PossiblePathesAPI.fetchAll();
 
-service.use(express.json())
+  const fastestRoute: Route = await new RouteOptimization(possiblePathes)
+    .useStrategy(new FastestStrategy())
+    .optimize(drone, start)
+    .optimize(start, end)
+    .get();
 
-service.post("", (request: Request, response: Response ) => {
-    response.status(200).json({
-        message: "function is on!"
-    })
-})
+
+  response.status(200).json(fastestRoute);
+});
 
 export const routeOptimization = functions
-                                    .region('southamerica-east1')
-                                    .https
-                                    .onRequest(service)
+  .region("southamerica-east1")
+  .https
+  .onRequest(service);
