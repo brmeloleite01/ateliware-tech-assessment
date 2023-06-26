7c2653a67cecb8c08d9df19b92531907e9748c90
brmeloleite01 <brmelo.leite@gmail.com>
2023-06-26 20:58:51 -0300

    http request with axios

--- backend/routeOptimization/src/service/PossiblePathesAPI.ts
+++ backend/routeOptimization/src/service/PossiblePathesAPI.ts
@@ -1,20 +1,12 @@
-import https from "https";
+import axios from "axios";
 import {PossiblePathes} from "../types/PossiblePathes";
 
 const endpoint = "https://mocki.io/v1/10404696-fd43-4481-a7ed-f9369073252f";
 
 export class PossiblePathesAPI {
   static async fetchAll(): Promise<PossiblePathes> {
-    return new Promise((resolve, reject) => {
-      https.get(endpoint, (res) => {
-        res.on("data", (data) => resolve(data as PossiblePathes));
-
-        res.on("error", (error) => {
-          console.log("Error", error);
-          reject(error);
-        });
-      });
-    });
+    const { data } =await axios.get(endpoint)
+    return data as PossiblePathes
   }
 }
 
